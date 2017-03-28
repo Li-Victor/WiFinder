5569d440e6b9d7e23d2e97bc0d6fb07e407c6387
victorli5674 <victorli5674@yahoo.com>
2017-03-28 15:41:44 -0700

    seperating hard coded data from the location info view

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -38,7 +38,46 @@ module.exports.homelist = function (req, res) {
 
 // GET 'Location info' page
 module.exports.locationInfo = function (req, res) {
-    res.render('location-info', { title: 'Location info' });
+    res.render('location-info', {
+        title: 'Location info',
+        pageHeader: {title: 'Starcups'},
+        sidebar: {
+            context: 'is on Wi-Finder because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            callToAction: 'If you\'re been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        location: {
+            name: 'Starcups',
+            address: '125 High Street, Reading RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: {lat: 51.455041, long: -0.9690884 },
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'George Lu',
+                rating: 5,
+                timestamp: 'March 20 2017',
+                reviewText: 'What a great place. I can\'t say enough good things about it.'
+            }, {
+                author: 'Kevin Wong',
+                rating: 3,
+                timestamp: 'March 22 2017',
+                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+            }]
+        }
+    });
   };
 
 // GET 'Add review' page
